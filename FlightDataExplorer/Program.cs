2b8db7e3eed2e90950b0824d31488632fd0751a5
E T <ellis.trickett@gmail.com>
2023-08-06 15:26:26 +0100

    Add Db Context with Connection String

--- FlightDataExplorer/Program.cs
+++ FlightDataExplorer/Program.cs
@@ -1,8 +1,26 @@
+using FlightDataExplorer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Load secrets for local development
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+builder.Services.AddDbContext<FlightDataContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("FlightDataContext")
+        .Replace("__ServerName__", builder.Configuration["FlightDataContext:Server"]);
+
+    options.UseSqlServer(connectionString);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,7 +32,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
+
+
 
 app.UseHttpsRedirection();
 
