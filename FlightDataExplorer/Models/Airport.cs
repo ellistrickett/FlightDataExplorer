5d8b7c77fcaf9c3b39c624e271b6b0339fab6b22
E T <ellis.trickett@gmail.com>
2023-08-06 13:48:18 +0100

    Define Data Models

---
+++ FlightDataExplorer/Models/Airport.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightDataExplorer.Models
+{
+    public class Airport
+    {
+        [Key]
+        public int AirportId { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string City { get; set; }
+
+        [Required]
+        public required string Country { get; set; }
+        [StringLength(3)]
+        public string? IATA { get; set; }
+        [StringLength(4)]
+        public string? ICAO { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Altitude { get; set; }
+        public double? Timezone { get; set; }
+
+        [Required]
+        public required string DST { get; set; }
+
+        [Required]
+        public required string? TzDatabaseTimeZone { get; set; }
+
+        [Required]
+        public required string Type { get; set; }
+
+        [Required]
+        public required string Source { get; set; }
+    }
+}
---
+++ FlightDataExplorer/Models/Flight.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightDataExplorer.Models
+{
+    public class Flight
+    {
+        [Key]
+        public int FlightId { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public required string Airline { get; set; }
+
+        public int? AirlineId { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public required string SourceAirport { get; set; }
+
+        public int? SourceAirportID { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public required string DestinationAirport { get; set; }
+
+        public int? DestinationAirportID { get; set; }
+        [StringLength(1)]
+        public string? Codeshare { get; set; }
+        public int Stops { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public required string Equipment { get; set; }
+    }
+}
